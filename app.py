67d234644aadc5c150c49e58ca2dedb49a41e310
jarjanazy <celil.cercenazi@gmail.com>
2021-01-09 00:42:02 +0300

    Create the webSocket app by using the socketIo library

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] ='thisIsMySecret' 
+
+socketIo = SocketIO(app)
 
 @app.route('/')
 def index():
     return "Hey man"
 
+@socketIo.on('message')
+def handleMessage(message):
+    print(message)
+    send(message, broadcast=True)
 
 if (__name__ == "__main__"):
-    app.run(debug=True)
+    socketIo.run(app)
